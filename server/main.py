865d309ca379e991d58b956a3f6334034ece0af5
MejarAdobo <momoy.dev@proton.me>
2026-03-06 11:36:12 -0800

    got it to get all the stations data

--- server/main.py
+++ server/main.py
@@ -1,13 +1,25 @@
 from parser import parse_station
 from scraper import fetch_station
 
+station_list = ["INANAI157", "INANAI140", "IGABRI5", "INANAI114", "INANAI143"]
+
+
+def gather_station_data():
+    station_report = []
+
+    for station in station_list:
+        url = f"https://www.wunderground.com/dashboard/pws/{station}"
+        html = fetch_station(url)
+
+        if html:
+            station_data = parse_station(html)
+            station_report.append(station_data)
+
+    return station_report
+
 
 def main():
-    url = "https://www.wunderground.com/dashboard/pws/IGABRI5"
-    html = fetch_station(url)
-
-    if html:
-        print(parse_station(html))
+    gather_station_data()
 
 
 if __name__ == "__main__":
