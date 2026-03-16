25371058b5573182e6c8f2a5e4e1d14460eaaf2b
MejarAdobo <momoy.dev@proton.me>
2026-03-16 12:01:36 -0700

    Client: Set up Tab Navigation for leaderboard, stations, records, and settings page

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -1,5 +1,38 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
-export default function RootLayout() {
-	return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="leaderboard"
+        options={{
+          title: "Leaderboard",
+          tabBarIcon: ({ color }) => <FontAwesome6 name="ranking-star" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="stations"
+        options={{
+          title: "Stations",
+          tabBarIcon: ({ color }) => <MaterialIcons name="location-on" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="records"
+        options={{
+          title: "Records",
+          tabBarIcon: ({ color }) => <MaterialIcons name="leaderboard" size={24} color="black" />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => <MaterialIcons name="settings" size={24} color="black" />,
+        }}
+      />
+    </Tabs>
+  );
 }
--- client/src/app/stations.tsx
+++ client/src/app/stations.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Station() {
+export default function Stations() {
   return (
     <View>
       <Text>Station</Text>
