c16eed8701a5bd8c87668e190b2740c44f92a653
MejarAdobo <momoy.dev@proton.me>
2026-03-18 11:45:53 -0700

    Client: Have Nativewind setup

--- client/src/app/(tabs)/leaderboard.tsx
+++ client/src/app/(tabs)/leaderboard.tsx
@@ -1,15 +1,15 @@
 import { Text, View } from "react-native";
 
 export default function Leaderboard() {
-	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<Text>Leaderboard</Text>
-		</View>
-	);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-2xl">Leaderboard</Text>
+    </View>
+  );
 }
