e305d2e4ea9790884e82a71fd761791fde3be074
MejarAdobo <momoy.dev@proton.me>
2026-04-09 12:42:57 -0700

    remove station page (out of scope)

--- client/src/app/(tabs)/_layout.tsx
+++ client/src/app/(tabs)/_layout.tsx
@@ -2,88 +2,73 @@ import { Tabs } from "expo-router";
 import { SymbolView } from "expo-symbols";
 
 export default function TabLayout() {
-	return (
-		<Tabs
-			screenOptions={{
-				headerShown: false,
-				tabBarStyle: {
-					position: "absolute",
-					borderTopLeftRadius: 30,
-					borderTopRightRadius: 30,
-					height: 100,
-					paddingTop: 10,
-					backgroundColor: "#efeae6",
-					shadowColor: "#000",
-					shadowOffset: { width: 0, height: 4 },
-					shadowOpacity: 0.5,
-					shadowRadius: 8,
-					elevation: 8,
-				},
-			}}
-		>
-			<Tabs.Screen
-				name="index"
-				options={{
-					title: "Leaderboard",
-					tabBarAccessibilityLabel: "Leaderboard",
-					tabBarActiveTintColor: "#291334",
-					tabBarIcon: ({ color }) => (
-						<SymbolView
-							name={{
-								android: "social_leaderboard",
-								web: "social_leaderboard",
-							}}
-							size={28}
-							tintColor={color}
-						/>
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="stations"
-				options={{
-					title: "Stations",
-					tabBarAccessibilityLabel: "Stations",
-					tabBarActiveTintColor: "#291334",
-					tabBarIcon: ({ color }) => (
-						<SymbolView
-							name={{ android: "location_on", web: "location_on" }}
-							size={28}
-							tintColor={color}
-						/>
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="records"
-				options={{
-					title: "Records",
-					tabBarAccessibilityLabel: "Records",
-					tabBarActiveTintColor: "#291334",
-					tabBarIcon: ({ color }) => (
-						<SymbolView
-							name={{ android: "leaderboard", web: "leaderboard" }}
-							size={28}
-							tintColor={color}
-						/>
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="settings"
-				options={{
-					title: "Settings",
-					tabBarAccessibilityLabel: "Settings",
-					tabBarActiveTintColor: "#291334",
-					tabBarIcon: ({ color }) => (
-						<SymbolView
-							name={{ android: "settings", web: "settings" }}
-							size={28}
-							tintColor={color}
-						/>
-					),
-				}}
-			/>
-		</Tabs>
-	);
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          height: 100,
+          paddingTop: 10,
+          backgroundColor: "#efeae6",
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.5,
+          shadowRadius: 8,
+          elevation: 8,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Leaderboard",
+          tabBarAccessibilityLabel: "Leaderboard",
+          tabBarActiveTintColor: "#291334",
+          tabBarIcon: ({ color }) => (
+            <SymbolView
+              name={{
+                android: "social_leaderboard",
+                web: "social_leaderboard",
+              }}
+              size={28}
+              tintColor={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="records"
+        options={{
+          title: "Records",
+          tabBarAccessibilityLabel: "Records",
+          tabBarActiveTintColor: "#291334",
+          tabBarIcon: ({ color }) => (
+            <SymbolView
+              name={{ android: "leaderboard", web: "leaderboard" }}
+              size={28}
+              tintColor={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarAccessibilityLabel: "Settings",
+          tabBarActiveTintColor: "#291334",
+          tabBarIcon: ({ color }) => (
+            <SymbolView
+              name={{ android: "settings", web: "settings" }}
+              size={28}
+              tintColor={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
