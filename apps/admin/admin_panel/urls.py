5efc36fb0c6d367368570d393463167d13df5a0e
MejarAdobo <momoy.dev@proton.me>
2026-07-04 10:07:14 -0700

    added a healtz route for admin panel so I can run a cron-job to keep it alive when I deploy in render

--- apps/admin/admin_panel/urls.py
+++ apps/admin/admin_panel/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
 urlpatterns = [
+    path("admin/healthz", lambda request: HttpResponse("OK")),
     path("admin/", admin.site.urls),
 ]
--- apps/admin/panel_app/apps.py
+++ apps/admin/panel_app/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PanelAppConfig(AppConfig):
-    name = 'panel_app'
+    name = "panel_app"
