32898b1879f22adf84a2006168bffc9389d2ad76
MejarAdobo <momoy.dev@proton.me>
2026-07-04 01:20:42 -0700

    make it so css show up in the render deploy admin panel app

--- apps/admin/admin_panel/settings.py
+++ apps/admin/admin_panel/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "admin_panel.urls"
@@ -123,4 +124,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
