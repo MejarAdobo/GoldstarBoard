3ddf2089615afdb7a7c8b193fc3fa66f76f1a0b9
MejarAdobo <momoy.dev@proton.me>
2026-07-06 12:48:49 -0700

    fix an issue of stats table not updating if their is no previous daily data for a station

--- apps/cron/src/tasks/stats.ts
+++ apps/cron/src/tasks/stats.ts
@@ -1,6 +1,8 @@
+import { getDailyDataByStation } from "@goldstarboard/db-services/dailyData/queries";
 import { updateStats as updtStats } from "@goldstarboard/db-services/stat/mutations";
 import { getYesterdayData } from "@helpers/getYesterday";
 import { Temporal } from "@js-temporal/polyfill";
+
 
 import type { Stats } from "@goldstarboard/shared-types/interfaces";
 
@@ -10,17 +12,33 @@ export const updateStats = async (stats: Stats[]) => {
       const yesterdayData = await getYesterdayData(stat.stationId);
       let { star, hotStreak, coldStreak, lastDaySinceStar } = stat;
 
-      if (yesterdayData?.starStatus == "gain" || yesterdayData?.starStatus == "maintain") {
-        star += 1;
-        hotStreak += 1;
-        coldStreak = 0;
-      } else if (yesterdayData?.starStatus == "loss" || yesterdayData?.starStatus == "none") {
-        coldStreak += 1;
-        hotStreak = 0;
+      if (yesterdayData) {
+        if (yesterdayData?.starStatus == "gain" || yesterdayData?.starStatus == "maintain") {
+          star += 1;
+          hotStreak += 1;
+          coldStreak = 0;
+        } else if (yesterdayData?.starStatus == "loss" || yesterdayData?.starStatus == "none") {
+          coldStreak += 1;
+          hotStreak = 0;
 
-        if (yesterdayData?.starStatus == "loss" && yesterdayData.createdAt) {
-          const instant = Temporal.Instant.from(yesterdayData.createdAt);
-          lastDaySinceStar = instant.toZonedDateTimeISO("UTC").toPlainDate().toString();
+          if (yesterdayData?.starStatus == "loss" && yesterdayData.createdAt) {
+            const instant = Temporal.Instant.from(yesterdayData.createdAt);
+            lastDaySinceStar = instant.toZonedDateTimeISO("UTC").toPlainDate().toString();
+          }
+        }
+      }
+      // This is if the station is recently added and have no previous daily data
+      else {
+        const dailyData = await getDailyDataByStation(stat.stationId);
+        const [recentDailyData] = dailyData.slice(-1);
+
+        if (recentDailyData) {
+          if (recentDailyData?.starStatus == "gain") {
+            star += 1;
+            hotStreak += 1;
+          } else {
+            coldStreak += 1;
+          }
         }
       }
 
