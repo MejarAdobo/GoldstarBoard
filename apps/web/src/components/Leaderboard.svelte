74342374dde3bd2b63ac0a3a86876f0fa11b736e
MejarAdobo <momoy.dev@proton.me>
2026-07-18 16:48:02 -0700

    working on web dev, decent enough to be deploy in render

--- apps/web/src/components/Leaderboard.svelte
+++ apps/web/src/components/Leaderboard.svelte
@@ -1,44 +1,44 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import LeaderboardList from './LeaderboardList.svelte';
-    import LeaderboardSwitch from './LeaderboardSwitch.svelte';
+  import { onMount } from "svelte";
+  import LeaderboardList from "./LeaderboardList.svelte";
+  import LeaderboardSwitch from "./LeaderboardSwitch.svelte";
 
-    let streakData = $state([]);
-    let starData = $state([]);
-    let hourlyData = $state([]);
-    let switchState = $state("streaks");
+  let streakData = $state([]);
+  let starData = $state([]);
+  let hourlyData = $state([]);
+  let switchState = $state("streaks");
 
-    const fetchAPI = async (url: string) => {
-      try {
-        const resp = await fetch(url)
-        return await resp.json()
-      } catch (error) {
-        console.error("Fetch failed:", error);
-        return [];
-      }
+  const fetchAPI = async (url: string) => {
+    try {
+      const resp = await fetch(url);
+      return await resp.json();
+    } catch (error) {
+      console.error("Fetch failed:", error);
+      return [];
     }
+  };
 
-    onMount(async () => {
-        const apiUrl = import.meta.env["PUBLIC_API_URL"]
-        const [streakResp, starResp, hourlyResp] = await Promise.all([
-                    fetchAPI(`${apiUrl}/streak-ranking`),
-                    fetchAPI(`${apiUrl}/star-ranking`),
-                    fetchAPI(`${apiUrl}/hourly-data`)
-        ]);
-        streakData = streakResp || [];
-        starData = starResp || [];
-        hourlyData = hourlyResp || [];
-    });
+  onMount(async () => {
+    const apiUrl = import.meta.env["PUBLIC_API_URL"];
+    const [streakResp, starResp, hourlyResp] = await Promise.all([
+      fetchAPI(`${apiUrl}/streak-ranking`),
+      fetchAPI(`${apiUrl}/star-ranking`),
+      fetchAPI(`${apiUrl}/hourly-data`),
+    ]);
+    streakData = streakResp || [];
+    starData = starResp || [];
+    hourlyData = hourlyResp || [];
+  });
+</script>
 
-    </script>
-
-<div class="max-w-xl mx-auto py-4 rounded-2xl bg-red-300 flex flex-col gap-4">
-    <div class="mx-4">
-        <LeaderboardSwitch bind:value={switchState} />
-    </div>
-    <div class="mx-4">
-        <LeaderboardList switchState={switchState} streakData={streakData} starData={starData} hourlyData={hourlyData} />
-    </div>
+<div class="mx-auto flex max-w-2xl flex-col gap-4 py-4">
+  <div class="flex justify-between">
+    <h2 class="text-4xl font-semibold text-text">{switchState === "streaks" ? "Streaks" : "Stars"}</h2>
+    <LeaderboardSwitch bind:value={switchState} />
+  </div>
+  <div>
+    <LeaderboardList {switchState} {streakData} {starData} {hourlyData} />
+  </div>
 </div>
 
 <!-- Plan: Header = Contain the Title (Streak, Most Star), and a switch to see either streak or star ranking, and last updated -->
