fb8846f8e5a5ae65748415dccbd301e0559a8443
MejarAdobo <momoy.dev@proton.me>
2026-07-23 16:20:02 -0700

    move the get stations and stats inside the cron job instead of outside

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -4,22 +4,22 @@ import { getDailyData, sendDailyData } from "@tasks/dailyData";
 import { getAllHourlyData, sendHourlyData } from "@tasks/hourlyData";
 import { updateStats } from "@tasks/stats";
 
-const stations = await getAllStations();
-const stats = await getAllStats();
-
 // Hourly job
 Bun.cron("@hourly", async () => {
+  const stations = await getAllStations();
   await sendHourlyData(await getAllHourlyData(stations));
 });
 
 // Daily job
 // This run at 6 in the morning at UTC, and run at near midnight in Pacific Time Zone
 Bun.cron("50 6 * * *", async () => {
+  const stations = await getAllStations();
   await sendDailyData(await getDailyData(stations));
 });
 
 // Stats job
 // This run at 6 in the morning at UTC, and run at near midnight in Pacific Time Zone
 Bun.cron("55 6 * * *", async () => {
+  const stats = await getAllStats();
   await updateStats(stats);
 });
