65625c53f2a403227f280a99366f81619b126879
MejarAdobo <momoy.dev@proton.me>
2026-08-10 22:07:44 +0000

    decided to remove admin app and just use db for crud operations since I can access with datagrip anyway

--- apps/admin/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-uv run manage.py makemigrations
-uv run manage.py migrate --noinput
-uv run manage.py collectstatic --noinput
-uv run manage.py createsuperuser --noinput || true
-exec uv run -m gunicorn --bind 0.0.0.0:${DJANGO_PORT} --workers 5 admin_panel.wsgi:application
--- apps/admin/panel_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PanelAppConfig(AppConfig):
-    name = "panel_app"
